24d1b3c66f8a3c6b5f10951a4939b288d60f80e5
codejs-net <support@code-js.net>
2021-07-19 01:01:25 +0530

    Update index.blade.php

--- resources/views/app/navigation/index.blade.php
+++ resources/views/app/navigation/index.blade.php
@@ -67,7 +67,7 @@ $name="name".$lang;
                             <div class="col ml-3">
                                 <a  class="filter_section" href="" data-toggle="collapse" data-target="#custom_item"><i class="fa fa-plus" aria-hidden="true"></i>&nbsp;<u>Custom Link</u></a>
                             </div>
-                            <div id="custom_item" class="collapse">
+                            <div id="custom_item" class="collapse show">
                                 <form method="post" id="custom_item_form" class="needs-validation"  novalidate>
                                     {{ csrf_field() }}
                                     <div class="row mx-2">
@@ -158,15 +158,15 @@ $name="name".$lang;
     }); 
 
     $(document).ready(function() {
+        $('#nestable').nestable('serialize');
         load_menu();
-        var updateOutput = function () {
-        $('#nestable-output').val(JSON.stringify($('#nestable').nestable('serialize')));
-        };
-
-        $('#nestable').nestable().on('change', updateOutput);
         updateOutput();
-        
+        $('#nestable').nestable().on('change', updateOutput);
     });
+
+    function updateOutput() {
+        $('#nestable-output').val(JSON.stringify($('#nestable').nestable('serialize')));
+    };
 
     $('#submenu').change(function() {
         if($('#submenu').prop("checked") == true){
@@ -241,7 +241,7 @@ $name="name".$lang;
         var op='';
         op+='<li class="dd-item dd3-item" data-id="' + id + '" data-label="' + label + '" data-url="' + url + '">';
         op+='<div class="dd-handle dd3-handle" > Drag</div>';
-        op+='<div class="dd3-content"><span>' + label + '</span><div class="item-edit">Edit</div></div>';
+        op+='<div class="dd3-content"><span>' + label + '</span><div class="item-edit"><i class="fa fa-pencil-square-o mr-1"></i>Edit</div></div>';
         op+='<div class="item-settings d-none">';
         op+='<p><label for="">Navigation Label<br><input type="text" name="navigation_label" value="' + label + '"></label></p>';
         op+='<p><label for="">Navigation Url<br><input type="text" name="navigation_url" value="' + url + '"></label></p>';
@@ -284,6 +284,29 @@ $name="name".$lang;
     }
 
     // -----nestable----------------------
+    $("body").delegate(".item-delete", "click", function (e) {
+        $(this).closest(".dd-item").remove();
+        updateOutput();
+    });
+
+
+    $("body").delegate(".item-edit, .item-close", "click", function (e) {
+        var item_setting = $(this).closest(".dd-item").find(".item-settings");
+        if (item_setting.hasClass("d-none")) {
+            item_setting.removeClass("d-none");
+        } else {
+            item_setting.addClass("d-none");
+        }
+    });
+
+    $("body").delegate("input[name='navigation_label']", "change paste keyup", function (e) {
+        $(this).closest(".dd-item").data("label", $(this).val());
+        $(this).closest(".dd-item").find(".dd3-content span").text($(this).val());
+    });
+
+    $("body").delegate("input[name='navigation_url']", "change paste keyup", function (e) {
+        $(this).closest(".dd-item").data("url", $(this).val());
+    });
     
 
 </script>
